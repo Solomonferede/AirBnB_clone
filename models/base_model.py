b0d31f5dddda0ebe140de62b993e9970a363cb56
solomon <solomonf1227@gmail.com>
2023-03-08 18:44:34 +0300

    From json to dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,25 +6,32 @@ defines all common attributes/methods for other classes:
 """
 
 
-import datetime
+from datetime import datetime
 import uuid
 
 
 class BaseModel:
     """defines all common attributes/methods for other classes:"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Public instance attributes: instantination"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>"""
         return "[" + self.__class__.__name__ + "] (" + str(self.id) + ") " + str(self.__dict__)
     def save(self):
         """Update the public instance attributes updated at"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
