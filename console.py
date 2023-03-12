1964f39ad96216d0d42536f92d82eda2fe816086
solomon <solomonf1227@gmail.com>
2023-03-12 09:47:23 +0300

    change excute permission

--- console.py
+++ console.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
+""" 
+the console module
 """
-======================================================
-Module with the entry point of the command interpreter
-======================================================
-"""
+
+
 import cmd
-import shlex
-import json
-from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-from models.state import State
-from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """command interpreter class"""
+    """The cmd interpretre class"""
 
-    # intro = 'Welcome the Airbnb console'
-    prompt = '(hbnb) '
-    classes = ["BaseModel", "User", "Place",  # List of classes we might need
-               "State", "City", "Amenity", "Review"]
+    prompt = "(hbnb) "
+    def do_prompt(self, line):
+        """ Change the defult prompt"""
+        self.prompt = line
 
-    def do_quit(self, arg):
-        """method for close and exit from the console"""
-
-        return True
-
-    def do_EOF(self, arg):
-        """method for exit from the console"""
-        print()
+    def do_quit(self, line):
+        """quit and EOF to exit the program"""
         exit()
+
+    def emptyline(self):
+        """Empty line"""
+        pass
+
+
+
 
 
 
